c71ecacfd364a839c4b920b8e49c44ff73fb3191
Miro <kolecani@gmail.com>
2024-09-02 17:41:36 +0200

    change

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from routers import root_index, pcb, buttons, text, searcher, arduino
+from routers import root_index, arduino
 # from .connector.database_connector import connection, cursor
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(root_index.router)
-app.include_router(pcb.router)
-app.include_router(buttons.router)
-app.include_router(text.router)
-app.include_router(searcher.router)
+
 app.include_router(arduino.router)
